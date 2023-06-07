17782f70d0dc5ac609cc024893f99e4fc761d92b
Boris Bonev <bbonev@nvidia.com>
2023-06-07 14:17:55 +0200

    minor bugfix in setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def version(root_path):
     ---------
     https://packaging.python.org/guides/single-sourcing-package-version/
     """
-    version_path = root_path.joinpath('neuralop', '__init__.py')
+    version_path = root_path.joinpath('torch_harmonics', '__init__.py')
     with version_path.open() as f:
         version_file = f.read()
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
