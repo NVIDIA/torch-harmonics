eb6800a40eb6638fb2411f5d50e9fe0bb4199a25
Thorsten Kurth <tkurth@nvidia.com>
2024-03-14 05:10:36 -0700

    reset setup.py

--- setup.py
+++ setup.py
@@ -83,7 +83,10 @@ def get_ext_modules(argv):
         sys.argv.remove("--cuda_ext")
         compile_cuda_extension = True
 
-    ext_modules = []
+    ext_modules = [
+        cpp_extension.CppExtension("disco_helpers", ["torch_harmonics/csrc/disco/disco_helpers.cpp"]),
+    ]
+
     if torch.cuda.is_available() or compile_cuda_extension:
         ext_modules.append(
             cpp_extension.CUDAExtension(
