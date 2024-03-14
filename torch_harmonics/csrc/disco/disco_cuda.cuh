6441e07b0727ea8f35059c2ac6e8b10d2ba182b5
Thorsten Kurth <tkurth@nvidia.com>
2024-03-14 05:23:02 -0700

    including CUDAStream

--- torch_harmonics/csrc/disco/disco_cuda.cuh
+++ torch_harmonics/csrc/disco/disco_cuda.cuh
@@ -33,7 +33,7 @@
 #include "disco.h"
 
 #include <cuda_runtime.h>
-#include <ATen/cudnn/Handle.h>  // for getcudnnhandle
+#include <c10/cuda/CUDAStream.h>
 
 #define CHECK_CUDA_TENSOR(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CUDA_INPUT_TENSOR(x) CHECK_CUDA_TENSOR(x); CHECK_CONTIGUOUS_TENSOR(x)
