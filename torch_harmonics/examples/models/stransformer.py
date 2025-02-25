c0f0f3dcd12369ce12cb7ba6be02b9bd1bf67a39
Boris Bonev <bbonev@nvidia.com>
2025-02-25 14:05:22 +0100

    fixing dependencies and removing * imports

--- torch_harmonics/examples/models/stransformer.py
+++ torch_harmonics/examples/models/stransformer.py
@@ -38,7 +38,7 @@ from torch_harmonics import DiscreteContinuousConvS2, DiscreteContinuousConvTran
 from torch_harmonics import NeighborhoodAttentionS2
 from torch_harmonics import ResampleS2
 
-from ._layers import *
+from ._layers import MLP
 
 from functools import partial
 
