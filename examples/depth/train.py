fixing data normalization for segmentation

examples/depth/train.py
@@ -52,11 +52,13 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-from torch_harmonics.examples import StanfordDepthDataset, Stanford2D3DSDownloader
+from torch_harmonics.examples import StanfordDepthDataset, Stanford2D3DSDownloader, compute_stats_s2
 from torch_harmonics.quadrature import _precompute_latitudes
 from torch_harmonics.examples.losses import L2LossS2
 from torch_harmonics.examples.metrics import RmseS2
 from torch_harmonics.plotting import plot_sphere, imshow_sphere
+
+from torchvision.transforms import v2
 
 # wandb logging
 import wandb
@@ -87,7 +89,8 @@ def log_weights_and_grads(exp_dir, model, iters=1):
 
 
 # rolls out the FNO and compares to the classical solver
-def validate_model(model, dataloader, loss_fn, metrics_fns, path_root, normalization=None, logging=True, device=torch.device("cpu")):
+def validate_model(model, dataloader, loss_fn, metrics_fns, path_root, normalization_in=None, normalization_out=None,
+                   logging=True, device=torch.device("cpu")):
 
     model.eval()
 
@@ -116,8 +119,11 @@ def validate_model(model, dataloader, loss_fn, metrics_fns, path_root, normaliza
             inp = inp.to(device)
             tar = tar.to(device)
 
-            if normalization is not None:
-                inp = normalization(inp)
+            if normalization_in is not None:
+                inp = normalization_in(inp)
+
+            if normalization_out is not None:
+                tar = normalization_out(tar)
 
             prd = model(inp)
 
@@ -158,7 +164,8 @@ def train_model(
     optimizer,
     gscaler,
     scheduler=None,
-    normalization=None,
+    normalization_in=None,
+    normalization_out=None,
     augmentation=None,
     nepochs=20,
     amp_mode="none",
@@ -197,8 +204,11 @@ def train_model(
             inp = inp.to(device)
             tar = tar.to(device)
 
-            if normalization is not None:
-                inp = normalization(inp)
+            if normalization_in is not None:
+                inp = normalization_in(inp)
+                
+            if normalization_out is not None:
+                tar = normalization_out(tar)
 
             if augmentation is not None:
                 inp = augmentation(inp)
@@ -249,8 +259,11 @@ def train_model(
                 inp = inp.to(device)
                 tar = tar.to(device)
 
-                if normalization is not None:
-                    inp = normalization(inp)
+                if normalization_in is not None:
+                    inp = normalization_in(inp)
+
+                if normalization_out is not None:
+                    tar = normalization_out(tar)
 
                 with torch.autocast(device_type="cuda", dtype=amp_dtype, enabled=(amp_mode != "none")):
                     prd = model(inp)
@@ -356,6 +369,12 @@ def main(
     dataset = StanfordDepthDataset(dataset_file=dataset_file, ignore_alpha_channel=ignore_alpha_channel)
     train_dataset, test_dataset, valid_dataset = torch.utils.data.random_split(dataset, split_ratios, generator=rng)
 
+    # stats computation
+    means_in, stds_in, means_out, stds_out = compute_stats_s2(train_dataset.dataset)
+    train_dataset.dataset.reset()
+    if logging:
+        print(f"Computed stats: means_in={means_in}, stds_in={stds_in}, means_out={means_out}, stds_out={stds_out}")
+
     # split dataset if distributed
     if dist.is_initialized():
         train_sampler = DistributedSampler(train_dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank(), shuffle=True, drop_last=True)
@@ -377,22 +396,15 @@ def main(
                                   num_workers=0, persistent_workers=False, pin_memory=True)
 
     # TODO: move augmentation into extra helper module
+    normalization_in = v2.Normalize(mean=means_in.tolist(), stds=stds_in.tolist())
+    normalization_out = v2.Normalize(mean=means_out.tolist(), stds=stds_out.tolist())
     if enable_data_augmentation:
         if not ignore_alpha_channel:
             raise NotImplementedError("You can only use data augmentation with RGB images, RGBA is not supported.")
         if logging:
             print("Using data augmentation")
-        from torchvision.transforms import v2
-
-        # get stas from file: WARNING! STATS HAVE BEEN COMPUTED OVER ALL SAMPLES
-        # NEED TO DISENTANGLE THAT CORRECTLY WITH STATIC SPLITS!
-        #mean = dataset.mean
-        #std = dataset.std
-        #print(f"Applying mean/variance normalization with mean={mean}, std={std}.")
-        #normalization = v2.Normalize(mean=mean, std=std)
 
         # imagenet normalization
-        normalization = v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         augmentation = v2.Compose(
             [
                 v2.RandomAutocontrast(p=0.5),
@@ -401,7 +413,6 @@ def main(
             ]
         )
     else:
-        normalization = None
         augmentation = None
 
     in_channels = 3 if ignore_alpha_channel else 4
@@ -552,7 +563,8 @@ def main(
                 optimizer,
                 gscaler,
                 scheduler,
-                normalization=normalization,
+                normalization_in=normalization_in,
+                normalization_out=normalization_out,
                 augmentation=augmentation,
                 nepochs=num_epochs,
                 amp_mode=amp_mode,
@@ -574,7 +586,8 @@ def main(
 
         with torch.inference_mode():
             losses, metric_results = validate_model(
-                model, valid_dataloader, loss_fn, metrics_fns, os.path.join(exp_dir, "figures"), normalization=normalization, logging=logging, device=device
+                model, valid_dataloader, loss_fn, metrics_fns, os.path.join(exp_dir, "figures"),
+                normalization_in=normalization_in, normalization_out=normalization_out, logging=logging, device=device
             )
             metrics[model_name]["loss_mean"] = torch.mean(losses)
             for metric in metric_results:

examples/segmentation/train.py
@@ -361,7 +361,7 @@ def main(
     train_dataset, test_dataset, valid_dataset = torch.utils.data.random_split(dataset, split_ratios, generator=rng)
 
     # compute stats on the train dataset
-    means, stds = compute_stats_s2(train_dataset.dataset)
+    means, stds = compute_stats_s2(train_dataset)
     train_dataset.dataset.reset()
     if logging:
         print(f"Computed stats: means={means}, stds={stds}")
@@ -427,8 +427,6 @@ def main(
     # make sure there is no nan
     if (class_weights is not None) and torch.isnan(class_weights).any():
         raise ValueError("The class weights contain NaN.")
-
-    # class_weights = None
 
     if logging:
         print(f"Train dataset initialized with {len(train_dataset)} samples of resolution {img_size}")
@@ -556,8 +554,8 @@ def main(
     #)
 
     # create the loss object
-    #loss_fn = CrossEntropyLossS2(nlat=img_size[0], nlon=img_size[1], grid="equiangular", weight=class_weights, smooth=label_smoothing).to(device=device)
-    loss_fn = DiceLossS2(nlat=img_size[0], nlon=img_size[1], grid="equiangular",  weight=class_weights, smooth=label_smoothing).to(device=device)
+    loss_fn = CrossEntropyLossS2(nlat=img_size[0], nlon=img_size[1], grid="equiangular", weight=class_weights, smooth=label_smoothing).to(device=device)
+    #loss_fn = DiceLossS2(nlat=img_size[0], nlon=img_size[1], grid="equiangular",  weight=class_weights, smooth=label_smoothing).to(device=device)
     # loss_fn = FocalLossS2(nlat=img_size[0], nlon=img_size[1], grid="equiangular").to(device=device)
 
     # compile loss function

torch_harmonics/examples/stanford_2d3ds_dataset.py
@@ -33,9 +33,10 @@ import os
 import math
 
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 import numpy as np
+import h5py as h5
 
 from torch_harmonics.quadrature import _precompute_latitudes
 from torch_harmonics.examples.losses import get_quadrature_weights
@@ -172,7 +173,6 @@ class Stanford2D3DSDownloader:
 
         from PIL import Image
         from tqdm import tqdm
-        import h5py as h5
 
         file_paths = []
 
@@ -242,6 +242,7 @@ class Stanford2D3DSDownloader:
             rgb_data = h5file.create_dataset("rgb", (num_samples, rgb_channels, *img_shape), "f4")
             semantic_data = h5file.create_dataset("semantic", (num_samples, *img_shape), "i8")
             depth_data = h5file.create_dataset("depth", (num_samples, *img_shape), "f4")
+            depth_mask = h5file.create_dataset("depth_mask", (num_samples, *img_shape), "i1")
             classes = h5file.create_dataset("class_labels", data=class_labels_indices)
             num_classes = len(set(class_labels_indices))
 
@@ -322,16 +323,20 @@ class Stanford2D3DSDownloader:
 
                 if downsampling_factor != 1:
                     dep = dep.resize(size=(img_shape[1], img_shape[0]), resample=Image.NEAREST)
-                dep_data = np.array(dep)    
-                depth_data[count, ...] = dep_data[...]
+                dep_data = np.array(dep)
+                mask = np.where(dep_data == 65535, 0, 1)
+                depth_data[count, ...] = dep_data[...] / 65536.0
+                depth_mask[count, ...] = mask[...]
 
                 # compute stats -> depth
                 # min/max
                 tmp_min_depth = np.min(dep_data, axis=(0, 1))
                 tmp_max_depth = np.max(dep_data, axis=(0, 1))
                 # mean/var
-                tmp_mean_depth = np.sum(dep_data * quad_weights[np.newaxis, :, :], axis=(1, 2))
-                tmp_m2_depth = np.sum(np.square(dep_data-tmp_mean_depth[:, np.newaxis, np.newaxis]) * quad_weights[np.newaxis, :, :])
+                tmp_mask = mask.astype(np.float64)
+                tmp_mask = mask / np.sum(mask)
+                tmp_mean_depth = np.sum(dep_data * quad_weights[:, :] * mask[:, :])
+                tmp_m2_depth = np.sum(np.square(dep_data-tmp_mean_depth) * quad_weights[:, :] * mask[:, :])
                 if count == 0:
                     min_vals_depth = tmp_min_depth
                     max_vals_depth = tmp_max_depth
@@ -407,8 +412,6 @@ class StanfordSegmentationDataset(Dataset):
         ignore_alpha_channel=True,
     ):
 
-        import h5py as h5
-
         self.dataset_file = dataset_file
 
         with h5.File(self.dataset_file, "r") as h5file:
@@ -455,7 +458,6 @@ class StanfordSegmentationDataset(Dataset):
         return self.num_samples
 
     def _init_files(self):
-        import h5py as h5
         self.h5file = h5.File(self.dataset_file, "r")
         self.class_labels = self.h5file["class_labels"]
         self.rgb = self.h5file["rgb"]
@@ -467,6 +469,8 @@ class StanfordSegmentationDataset(Dataset):
         self.semantic = None
         if self.h5file is not None:
             self.h5file.close()
+            del self.h5file
+        self.h5file = None
 
     def __getitem__(self, idx, mask_invalid=True):
 
@@ -500,13 +504,12 @@ class StanfordDepthDataset(Dataset):
         ignore_alpha_channel=True,
     ):
 
-        import h5py as h5
-
         self.dataset_file = dataset_file
 
         with h5.File(self.dataset_file, "r") as h5file:
             self.img_rgb = h5file["rgb"][0].shape
             self.img_depth = h5file["depth"][0].shape
+            self.img_mask = h5file["mask"][0].shape
             self.num_samples = h5file["rgb"].shape[0]
             
             self.mean_in = h5file["mean_rgb"][...]
@@ -526,6 +529,7 @@ class StanfordDepthDataset(Dataset):
         self.h5file = None
         self.rgb = None
         self.depth = None
+        self.mask = None
 
     @property
     def target_shape(self):
@@ -539,16 +543,19 @@ class StanfordDepthDataset(Dataset):
         return self.num_samples
 
     def _init_files(self):
-        import h5py as h5
         self.h5file = h5.File(self.dataset_file, "r")
         self.rgb = self.h5file["rgb"]
         self.depth = self.h5file["depth"]
+        self.mask = self.h5file["depth_mask"]
 
     def reset(self):
         self.rgb = None
         self.depth = None
+        self.mask = None
         if self.h5file is not None:
             self.h5file.close()
+            del self.h5file
+        self.h5file = None
 
     def __getitem__(self, idx):
 
@@ -558,8 +565,9 @@ class StanfordDepthDataset(Dataset):
 
         rgb = self.rgb[idx, 0 : self.img_rgb[0], 0 : self.img_rgb[1], 0 : self.img_rgb[2]]
         depth = self.depth[idx, 0 : self.img_depth[0], 0 : self.img_depth[1]]
+        mask = self.mask[idx, 0 : self.img_mask[0], 0 : self.img_mask[1]].astype(np.float32)
 
-        return rgb, depth
+        return rgb, depth, mask
 
 
 
@@ -567,29 +575,36 @@ def compute_stats_s2(dataset: Dataset):
 
     nexamples = len(dataset)
     for count in range(nexamples):
-        inp, tar = dataset[i]
+        token = dataset[count]
+        
+        if isinstance(dataset, StanfordDepthDataset):
+            inp, tar, mask = token
+        else:
+            inp, tar = token
 
         if count == 0:
-            quadrature_weights = get_quadrature_weights(nlat=inp.shape[1], nlon=inp.shape[2], grid="equiangular", tile=True).numpy().astype(np.float64)
+            quad_weights = get_quadrature_weights(nlat=inp.shape[1], nlon=inp.shape[2], grid="equiangular", tile=True).numpy().astype(np.float64)
         
         # do welford update
         if count == 0:
-            rgb_means = np.sum(inp * quadrature_weights[np.newaxis, :, :], axis=(1,2))
+            rgb_means = np.sum(inp * quad_weights[np.newaxis, :, :], axis=(1,2))
             rgb_stds = np.sum(np.square(inp-rgb_means[:, np.newaxis, np.newaxis]) * quad_weights[np.newaxis, :, :], axis=(1,2))
             if isinstance(dataset, StanfordDepthDataset):
-                depth_means = np.sum(tar * quadrature_weights[np.newaxis, :, :], axis=(1,2))
-                depth_stds = np.sum(np.square(tar-depth_means[:, np.newaxis, np.newaxis]) * quad_weights[np.newaxis, :, :], axis=(1,2))
+                mask = mask / np.sum(mask)
+                depth_means = np.sum(tar * quad_weights[:, :] * mask)
+                depth_stds = np.sum(np.square(tar-depth_means) * quad_weights[:, :] * mask)
 
         else:
-            means = np.sum(inp * quadrature_weights[np.newaxis, :, :], axis=(1,2))
+            means = np.sum(inp * quad_weights[np.newaxis, :, :], axis=(1,2))
             m2s = np.sum(np.square(inp-means[:, np.newaxis, np.newaxis]) * quad_weights[np.newaxis, :, :], axis=(1,2))
             delta = means - rgb_means
             rgb_means += delta / float(count + 1)
             rgb_stds += m2s + delta * delta * float(count / (count + 1))
 
             if isinstance(dataset, StanfordDepthDataset):
-                means = np.sum(tar * quadrature_weights[np.newaxis, :, :], axis=(1,2))
-                m2s = np.sum(np.square(tar-means[:, np.newaxis, np.newaxis]) * quad_weights[np.newaxis, :, :], axis=(1,2))
+                mask = mask / np.sum(mask)
+                means = np.sum(tar * quad_weights[:, :] * mask)
+                m2s = np.sum(np.square(tar-means) * quad_weights[:, :] * mask)
                 delta = means - depth_means
                 depth_means += delta / float(count + 1)
                 depth_stds += m2s + delta * delta * float(count / (count + 1))