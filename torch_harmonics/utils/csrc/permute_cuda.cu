50f478ce884696a6a66336a0fefdc0a1606c7fc0
Thorsten Kurth <tkurth@nvidia.com>
2025-08-29 03:27:42 -0700

    fixed a lot of issues

--- torch_harmonics/utils/csrc/permute_cuda.cu
+++ torch_harmonics/utils/csrc/permute_cuda.cu
@@ -40,79 +40,15 @@
 #include <limits>
 
 #include "cudamacro.h"
-#include "cuda_utils.cuh"
+#include "permute_cuda.cuh"
 
-#define THREADS (64)
-
+// Define the missing macros
 #define TRANSP_WARPS_X_TILE_GENERIC (32)
 #define TRANSP_WARPS_X_TILE_SM100    (4)
 
 namespace utility_kernels {
 
-// BEGIN - CSR rows sorting kernels and functions
-__global__ void set_rlen_rids_k(const int n,
-                                const int64_t *__restrict__ offs,
-                                      int *__restrict__ rids,
-                                      int *__restrict__ rlen) {
-
-    const int nth = gridDim.x*blockDim.x;
-    const int tid = blockIdx.x*blockDim.x + threadIdx.x;
-
-    for(int i = tid; i < n; i += nth) {
-        rids[i] = i;
-        rlen[i] = offs[i+1]-offs[i];
-    }
-
-    return;
-}   
-
-torch::Tensor sortRows(int nlat_out, torch::Tensor row_off, cudaStream_t stream) {
-
-    int64_t *_row_off_d = reinterpret_cast<int64_t *>(row_off.data_ptr());
-
-    auto options = torch::TensorOptions().dtype(torch::kInt32).device(row_off.device());
-
-    torch::Tensor rids_d = torch::empty({nlat_out}, options);
-    torch::Tensor rlen_d = torch::empty({nlat_out}, options);
-
-    int *_rids_d = reinterpret_cast<int *>(rids_d.data_ptr());
-    int *_rlen_d = reinterpret_cast<int *>(rlen_d.data_ptr());
-
-    const int grid = DIV_UP(nlat_out, THREADS);
-    const int block = THREADS;
-
-    set_rlen_rids_k<<<grid, block, 0, stream>>>(nlat_out,
-                                                _row_off_d,
-                                                _rids_d,
-                                                _rlen_d);
-
-    torch::Tensor rids_sort_d = torch::empty({nlat_out}, options);
-    torch::Tensor rlen_sort_d = torch::empty({nlat_out}, options);
-
-    int *_rids_sort_d = reinterpret_cast<int *>(rids_sort_d.data_ptr());
-    int *_rlen_sort_d = reinterpret_cast<int *>(rlen_sort_d.data_ptr());
-
-    size_t temp_storage_bytes = 0;
-    CHECK_CUDA(cub::DeviceRadixSort::SortPairsDescending(NULL, temp_storage_bytes,
-                                                         _rlen_d, _rlen_sort_d, 
-                                                         _rids_d, _rids_sort_d,
-                                                         nlat_out, 0, sizeof(*_rlen_d)*8, stream));
-
-    options = torch::TensorOptions().dtype(torch::kByte).device(row_off.device());
-    torch::Tensor temp_storage_d = torch::empty({int64_t(temp_storage_bytes)}, options);
-
-    void *_temp_storage_d = reinterpret_cast<void *>(temp_storage_d.data_ptr());
-
-    CHECK_CUDA(cub::DeviceRadixSort::SortPairsDescending(_temp_storage_d, temp_storage_bytes,
-                                                         _rlen_d, _rlen_sort_d, 
-                                                         _rids_d, _rids_sort_d,
-                                                         nlat_out, 0, sizeof(*_rlen_d)*8, stream));
-    return rids_sort_d;
-}
-// END - CSR rows sorting kernels and functions
-
-
-// BEGIN - 4D tensor permutation kernels and functions
+    // BEGIN - 4D tensor permutation kernels and functions
 __global__ void empty_k() {}
 
 static int getPtxver() {
@@ -121,7 +57,7 @@ static int getPtxver() {
     return attrs.ptxVersion*10;
 }
 
-at::Tensor permute_4D_to0231(at::Tensor src) {
+torch::Tensor permute_4D_to0231(torch::Tensor src) {
 
     auto options = torch::TensorOptions().dtype(src.dtype()).device(src.device());
     torch::Tensor dst = torch::empty({src.size(0), src.size(2), src.size(3), src.size(1)}, options);
@@ -144,7 +80,7 @@ at::Tensor permute_4D_to0231(at::Tensor src) {
     return dst;
 }
 
-at::Tensor permute_4D_to0312(at::Tensor src) {
+torch::Tensor permute_4D_to0312(torch::Tensor src) {
 
     auto options = torch::TensorOptions().dtype(src.dtype()).device(src.device());
     torch::Tensor dst = torch::empty({src.size(0), src.size(3), src.size(1), src.size(2)}, options);
@@ -166,25 +102,13 @@ at::Tensor permute_4D_to0312(at::Tensor src) {
 
     return dst;
 }
-// END - tensor permutation kernels and functions
-
-// BEGIN - general host-side functions
-unsigned int next_pow2(unsigned int x) { 
-
-    x -= 1;
-
-    #pragma unroll
-    for(int i = 1; i <= sizeof(x)*8 / 2; i *= 2) {
-        x |= x >> i;    
-    }
-    return x+1;
-}
-// END - general host-side functions
 
 TORCH_LIBRARY_IMPL(utility_kernels, CUDA, m)
-    {
-        m.impl("permute_0231",  &permute_4D_to0231);
-        m.impl("permute_0312",  &permute_4D_to0312);
-    }
+{
+    m.impl("permute_to_0231",  &permute_4D_to0231);
+    m.impl("permute_to_0312",  &permute_4D_to0312);
+}
+
+// END - tensor permutation kernels and functions
 
 }
