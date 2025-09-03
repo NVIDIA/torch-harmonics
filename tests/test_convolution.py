bc17354d0dfbcf9728a3f4adf7bf1a1f86ab365c
Thorsten Kurth <tkurth@nvidia.com>
2025-09-02 23:44:45 -0700

    adding reshapes

--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -326,6 +326,7 @@ class TestDiscreteContinuousConvolution(unittest.TestCase):
         x_ref_grad = x_ref.grad.clone()
 
         # compare results
+        print(y, y_ref)
         self.assertTrue(torch.allclose(y, y_ref, rtol=tol, atol=tol))
 
         # compare
