483834e7cbc192b339b015e0f7c2f683561c3361
Thorsten Kurth <tkurth@nvidia.com>
2026-04-22 02:06:34 -0700

    adding CPU upsampling backend

--- torch_harmonics/attention/__init__.py
+++ torch_harmonics/attention/__init__.py
@@ -41,4 +41,4 @@ else:
     attention_kernels = None
     warnings.warn("No optimized kernels are available. Please compile the extension first setting BUILD_CPP and BUILD_CUDA to 1.")
 
-from .attention import AttentionS2, NeighborhoodAttentionS2
+from .attention import AttentionS2, NeighborhoodAttentionS2, NeighborhoodAttentionUpsampleS2
