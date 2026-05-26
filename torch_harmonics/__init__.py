c64a0da69322710662a0c0676358ca4e9467d9bc
Thorsten Kurth <tkurth@nvidia.com>
2026-05-26 11:35:47 +0200

    bumping version number and adding changelog

--- torch_harmonics/__init__.py
+++ torch_harmonics/__init__.py
@@ -29,7 +29,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-__version__ = "0.9.1"
+__version__ = "0.9.2a"
 
 from . import examples, quadrature, random_fields
 from .attention import AttentionS2, NeighborhoodAttentionS2
